0305111dc1b8bf52df8e9fae0a2a1b784a99f1bc
Ivan0707z <112898142+Ivan0707z@users.noreply.github.com>
2022-09-13 10:34:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Лабараторна робота 1 Заїчко Іван
+using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("20 random numbers 0 to 100");
+            Console.WriteLine("---");
+
+            Random random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(random.Next(0, 101));
+                Console.Write("");
+
+            }
+            Console.WriteLine("");
+        }
+
+    }
+}
